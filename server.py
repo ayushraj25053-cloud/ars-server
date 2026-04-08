7ac2346b8dc93c3071f743f9c311620bc3bdb439
ayushraj25053-cloud <ayushraj25053@gmail.com>
2026-04-08 14:21:52 +0530

    Update server.py

--- server.py
+++ server.py
@@ -170,4 +170,6 @@ def api_check():
             }
 
     return {"status": "INVALID"}
-app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
